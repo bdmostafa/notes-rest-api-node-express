9da9abaa5546f832835a0dacb1e3cfae5b4728e2
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-10-22 19:17:20 +0600

    delete operation

--- index.js
+++ index.js
@@ -81,34 +81,54 @@ app.put('/notes/:noteId', (req, res) => {
     const allowedForUpdates = ['title', 'description'];
     const isAllowed = keysInput.every(update => allowedForUpdates.includes(update))
 
-    if (!isAllowed){
+    if (!isAllowed) {
         return res.status(400).send('Invalid Update Operation.')
     }
     // Check if note exists
     const note = notes.find(note => note.id === noteId)
-    console.log(note)
-    if (note) {
-        // Update here
-        notes = notes.map(note => {
-            if (note.id === noteId) {
-                return {
-                    ...note,
-                    ...noteInput
-                };
-            } else {
-                return note;
-            }
-        });
-        return res.send(notes)
-    } else {
-        // Note not found
-        return res.status(404).send('Note Not Found.')
+    try {
+        if (note) {
+            // Update here
+            notes = notes.map(note => {
+                if (note.id === noteId) {
+                    return {
+                        ...note,
+                        ...noteInput
+                    };
+                } else {
+                    return note;
+                }
+            });
+            return res.send(notes)
+        } else {
+            // Note not found
+            return res.status(404).send('Note Not Found.')
+        }
+    } catch (err) {
+        // Server error
+        res.status(500).send('Internal Server Error.')
     }
-    // Server error
-    res.status(500).send('Internal Server Error.')
 })
 
 
+// Delete notes
+app.delete('/notes/:noteId', (req, res) => {
+    const noteId = parseInt(req.params.noteId);
+    try {
+        // Check if note exists
+        const note = notes.find(note => note.id === noteId)
+        if (note) {
+            // Delete Note
+            notes = notes.filter(note => note.id !== noteId);
+            res.send(notes)
+        } else {
+            // Note Not Found
+            res.status(404).send('Note Not Found')
+        }
+    } catch (err) {
+        res.status(500).send('Internal Server Error.')
+    }
+})
 
 
 // create server

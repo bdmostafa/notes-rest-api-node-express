e060520800409979b2a44db95143c64445c1b665
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-10-26 13:17:24 +0600

    enable cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 // Port
 const PORT = process.env.PORT || 4000;
@@ -13,30 +14,31 @@ require('dotenv').config({
 // Middleware
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIES_SECRET));
+app.use(cors());
 
 // Import Error middleware
-const {error} = require('./middleware/error')
+const {error} = require('./middleware/error');
 
 // Import DB
-const { connectDB } = require('./db/dbConnection')
+const { connectDB } = require('./db/dbConnection');
 
 // Connecting DB
 connectDB();
 
 // Import Routes
-const indexRoute = require('./routes')
-const notesRoute = require('./routes/notes')
-const usersRoute = require('./routes/users')
+const indexRoute = require('./routes');
+const notesRoute = require('./routes/notes');
+const usersRoute = require('./routes/users');
 
 
 // Handling Routes
-app.use('/notes', notesRoute)
-app.use('/users', usersRoute)
-app.use('/', indexRoute)
-app.use(error)
+app.use('/notes', notesRoute);
+app.use('/users', usersRoute);
+app.use('/', indexRoute);
+app.use(error);
 
 
 // create server
 app.listen(PORT, () => {
-    console.log(`Listening on the ${PORT}`)
+    console.log(`Listening on the ${PORT}`);
 })
